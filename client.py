01d46ca638214d1a7dc866c0a2ed9b683dceafd7
Vegard Botnen <vgardb@gmail.com>
2018-02-07 11:00:17 +0100

    Working client example.

--- client.py
+++ client.py
@@ -1,23 +1,48 @@
 import socket
- 
-def Main():
-    host = "127.0.0.1"
-    port = 5000
-     
-    mySocket = socket.socket()
-    mySocket.bind((host,port))
-     
-    mySocket.listen(1)
-    conn, addr = mySocket.accept()
-    print ("Connection from: " + str(addr))
-    while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                    break
-            print ("from connected  user: " + str(data))
-             
-            data = str(data).upper()
-            print ("sending: " + str(data))
-            conn.send(data.encode())
-             
-    conn.close()s
+
+
+# implement HTTP
+# def Main():
+#     host = "127.0.0.1"
+#     port = 5000
+
+#     mySocket = socket.socket()
+#     mySocket.bind((host,port))
+
+#     mySocket.listen(1)
+#     conn, addr = mySocket.accept()
+#     print ("Connection from: " + str(addr))
+#     while True:
+#             data = conn.recv(1024).decode()
+#             if not data:
+#                     break
+#             print ("from connected  user: " + str(data))
+
+#             data = str(data).upper()
+#             print ("sending: " + str(data))
+#             conn.send(data.encode())
+
+#     conn.close()
+
+
+# without HTTP
+def main():
+    host = '127.0.0.1'
+    port = 8001
+
+    # connect to server
+    sock = socket.socket()
+    sock.connect((host, port))
+
+    message = input("--> ")
+
+    while message != "q":
+        sock.send(message.encode('utf-8'))
+        data = sock.recv(1024).decode('utf-8')
+        print("Recieved from server:", data)
+        message = input("--> ")
+
+    sock.close()
+
+
+main()
