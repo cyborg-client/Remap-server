dba57376116e0da33becb688ec13c08523af8eae
Johan Lofstad <johan.lofstad@gmail.com>
2018-02-21 14:39:21 +0100

    Added diagram and continued work on http to mea

--- goclient/main.go
+++ goclient/main.go
@@ -13,13 +13,20 @@ func main() {
 	 clientRequestCh := make(chan dataTypes.ClientRequest)
 
 	 go tcpHttpClient.TcpHttpClientMain(tcpDataStreamCh, tcpHttpClientStatusCh, clientRequestCh)
-	
+
+	 testConfig := make(map[string]int)
+	 testConfig["sample_rate"] = 1000
+	 testConfig["segment_length"] = 10
+
+	 myReq := dataTypes.ClientRequest{Request:dataTypes.Start, Options:testConfig}
+	 clientRequestCh <- myReq
+	 /*
 	 myReq := dataTypes.ClientRequest{Request:dataTypes.Start}
 	 myReqStop := dataTypes.ClientRequest{Request:dataTypes.Stop}
 	 clientRequestCh<-myReq
 	 clientRequestCh<-myReqStop
 	 clientRequestCh<-myReq
 	 clientRequestCh<-myReqStop
-
+*/
 	 select{}
 }
--- goclient/tcpHttpClient/http.go
+++ goclient/tcpHttpClient/http.go
@@ -7,36 +7,50 @@ import (
 	"net/http"
 	"log"
 	"bytes"
+	"encoding/json"
 )
 
-func requestRemoteServer(start bool) (bool) {
+// Struct representing the data which is sent to the MEA Server. 
+type requestOptions struct {
+	SampleRate int `json:"sample_rate""`
+	SegmentLength int `json:"segment_length""`
+}
+
+func requestRemoteServer(start bool, sampleRate int, segmentLength int) (bool) {
 	var url string
 	if start {
 		url = "http://" + config.MEAServerAddress + "/start"
 	}else {
 		url = "http://" + config.MEAServerAddress + "/stop"
 	}
-	var jsonStr = []byte(`{"sample_rate":1000, "segment_length":10}`)
-	var buf *bytes.Buffer
-	if(start){
-		buf = bytes.NewBuffer(jsonStr)
-	}else{
-		buf = bytes.NewBuffer([]byte(``))
+
+	// Generate json
+	reqOption := requestOptions{SampleRate:sampleRate, SegmentLength:segmentLength}
+	b, err := json.Marshal(reqOption)
+	if err != nil {
+		// TODO: Handle error
+		fmt.Println("error:", err)
 	}
+
+	var jsonStr = []byte(b)
+	buf := bytes.NewBuffer(jsonStr)
 	resp, err := http.Post(url, "application/json", buf)
 	if err != nil {
 		// Error
+		// TODO : Handle error
 		log.Println("Error in http.go: ", err)
 		return false
 	}
 	defer resp.Body.Close() // Close HTTP when done
 
+	// DEBUG
+	// TODO: Remove debug when the communication with the server is functioning
 	bufT := new(bytes.Buffer)
 	bufT.ReadFrom(resp.Body)
 	newStr := bufT.String()
 	fmt.Println(newStr)
-
 	fmt.Println(resp.StatusCode)
+
 	if resp.StatusCode == 200 {
 		return true
 	} else {
@@ -55,12 +69,12 @@ func httpMain(
 		case req := <-clientRequestCh:
 			if req.Request == dataTypes.Start {
 				fmt.Println("start")
-				if requestRemoteServer(true){
+				if requestRemoteServer(true, req.Options["sample_rate"], req.Options["segment_length"]){
 				}
 
 			} else if req.Request == dataTypes.Stop {
 				fmt.Println("Stop")
-				requestRemoteServer(false)
+				requestRemoteServer(false, 0, 0)
 			}
 		}
 	}
