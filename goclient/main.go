380790c2faf334f685b671cc21b98c9a479b7a34
Vegard Botnen <vgardb@gmail.com>
2018-02-21 13:11:37 +0100

    Running robotserver.

--- goclient/main.go
+++ goclient/main.go
@@ -1,23 +1,24 @@
 package main
 
-import
-(
-	"./dataTypes"
-	"./tcpHttpClient"
+import (
+	"./robotserver"
 )
 
 func main() {
-	// Make channels
-	 tcpDataStreamCh := make(chan dataTypes.TcpDataStream)
-	 tcpHttpClientStatusCh := make(chan dataTypes.TcpHttpClientStatus)
-	 clientRequestCh := make(chan dataTypes.ClientRequest)
+	// // Make channels
+	// tcpDataStreamCh := make(chan dataTypes.TcpDataStream)
+	// tcpHttpClientStatusCh := make(chan dataTypes.TcpHttpClientStatus)
+	// clientRequestCh := make(chan dataTypes.ClientRequest)
 
-	 go tcpHttpClient.TcpHttpClientMain(tcpDataStreamCh, tcpHttpClientStatusCh, clientRequestCh)
-	 myReq := dataTypes.ClientRequest{Request:dataTypes.Start}
-	 myReqStop := dataTypes.ClientRequest{Request:dataTypes.Stop}
-	 clientRequestCh<-myReq
-	 clientRequestCh<-myReqStop
-	 clientRequestCh<-myReq
-	 clientRequestCh<-myReqStop
-	 select{}
+	// go tcpHttpClient.TcpHttpClientMain(tcpDataStreamCh, tcpHttpClientStatusCh, clientRequestCh)
+	// myReq := dataTypes.ClientRequest{Request: dataTypes.Start}
+	// myReqStop := dataTypes.ClientRequest{Request: dataTypes.Stop}
+	// clientRequestCh <- myReq
+	// clientRequestCh <- myReqStop
+	// clientRequestCh <- myReq
+	// clientRequestCh <- myReqStop
+	// select {}
+
+	// Robot Server
+	robotserver.RobotServerMain()
 }
