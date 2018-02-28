2325728cf40d3f7365f685e470f29cba5b002fb1
Vegard Botnen <vgardb@gmail.com>
2018-02-28 14:36:14 +0100

    Output when starting server.

--- robotserver/server.go
+++ robotserver/server.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// GetData parses REST request
+func GetData(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range params {
+		// if param exist, check whether it's an integer
+		everyMs, err := strconv.Atoi(params["every-ms"])
+		if err != nil {
+			fmt.Printf("Error! Invalid 'every-ms' type, should be integer, found: '%v'\n", item)
+		} else {
+			// send request to buffer-package
+			// TODO: call buffer.GetDataEveryMs(everyMs, ...)
+			fmt.Printf("Receiving data request for every %v ms\n", everyMs)
+		}
+	}
 }
 
-func RobotServerMain() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+// Main is main func in robotserver-package
+func Main(port string) {
+	router := mux.NewRouter()
+	router.HandleFunc("/data/{every-ms}", GetData).Methods("GET")
+	fmt.Printf("Starting localhost at port: %v. REST: '/data/<millisec>'\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
