abae038edac778e184e3d737454b860dcc5ba903
Vegard Botnen <vgardb@gmail.com>
2018-03-07 13:34:22 +0100

    Jonas' parser.

---
+++ analysis/dataParser.go
@@ -0,0 +1,68 @@
+package analysis
+
+import (
+	"fmt"
+	"os"
+	"encoding/csv"
+	"bufio"
+	"io"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// TODO: find out how to syncronise all channels
+func Main(timeStampChannel chan<- []byte) {
+	dat, err := os.Open("/Users/jonasdammen/Development/code/src/github.com/cyborg-client/client/sampledata/2017-10-20_MEA2_100rows_1sec.csv")
+	check(err)
+	for {
+		reader := csv.NewReader(bufio.NewReader(dat))
+		var timestampArray= make([][]byte, 0, 10)
+		i := 0
+		cols := 0
+		reader.Read() // first line is info only
+		for {
+			record, err := reader.Read()
+			if i == 0 {
+				cols = len(record)
+			}
+			timestampArray = append(timestampArray, make([]byte, cols-1))
+			// Stop at EOF.
+			if err == io.EOF {
+				fmt.Println("Breaking due to EOF")
+				break
+			}
+			//fmt.Println("reading values")
+
+			for j := range record {
+				if j != 0 { // first value is timestamp
+					val, _ := strconv.Atoi(record[j])
+					if val < 0 {
+						timestampArray[i][j-1] = 1
+
+					} else {
+						timestampArray[i][j-1] = 0
+					}
+				}
+			}
+			/*
+		fmt.Println("Record current line")
+		fmt.Println((record))
+		fmt.Println("TSA current line")
+		fmt.Println(timestampArray[i])
+		fmt.Println("TSA current length")
+		fmt.Println(len(timestampArray))
+		*/
+			//fmt.Println("Pushing one array to channel")
+			//fmt.Println(timestampArray[i])
+			timeStampChannel <- timestampArray[i]
+			i++
+		}
+		fmt.Println("Sent ", i, " lines");
+		//check(err)	r := csv.NewReader(strings.NewReader(in))
+	}
+}
